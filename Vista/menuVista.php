87cfd832c0f65298425a103d9d590f5580794b1c
andreazunino <108498948+andreazunino@users.noreply.github.com>
2024-02-20 09:13:00 -0300

    Nuevo mnenu vista incluyendo perfiles de profesor, alumno y administrador

---
+++ Vista/menuVista.php
@@ -0,0 +1,63 @@
+<?php
+
+class Vista {
+   
+    public function mostrarLogin() {
+        $opcionesMenuPrincipal = [
+            "Soy Estudiante",
+            "Soy Profesor",
+            "Soy Administrador"
+        ];
+        $this->mostrarMenu($opcionesLogin);
+    }
+
+    public function mostrarMenuEstudiantes() {
+        $opcionesEstudiantes = ["Inscribirse a curso", "Anular inscripcion a curso", "Ver cursos inscripto"];
+        $this->mostrarMenu($opcionesEstudiantes);
+    }
+
+    public function mostrarMenuProfesores(){
+        $opcionesProfesores = ["Cargar notas", "Ver notas de alumnos", "Ver Cursos"];
+        $this->mostrarMenu($opcionesProfesores);
+    }
+
+
+    public function mostrarMenuAdministradores(){
+        $opcionesAdministradores = ["Administración de Estudiantes", "Administración de Cursos", "Administración de Inscripciones"];
+        $this->mostrarMenu($opcionesMenuAdministradores);
+
+    }
+
+    public function mostrarSubMenuEstudiantes() {
+        $opcionesUsuarios = ["Dar de Alta", "Dar de Baja", "Modificar Datos", "Ver Datos e Inscripciones"];
+        $this->mostrarMenu($opcionesUsuarios);
+    } 
+
+    public function mostrarSubMenuCursos() {
+        $opcionesCursos = ["Dar de Alta", "Dar de Baja", "Modificar Datos", "Listar"];
+        $this->mostrarMenu($opcionesCursos);
+    }
+
+    public function mostrarSubMenuInscripciones() {
+        $opcionesInscripciones = ["Inscribir", "Borrar Inscripcion", "Listar Inscripciones"];
+        $this->mostrarMenu($opcionesInscripciones);
+    }
+
+    public function mostrarMensajeError($mensaje) {
+        echo "Error: " . $mensaje . "\n";
+    }
+
+      private function mostrarMenu(array $opciones) {
+        echo "=========== Bienvenido ==========\n";
+        echo "============= Menú ==============\n";
+        foreach ($opciones as $index => $opcion) {
+            printf("%-2s. %s\n", $index + 1, $opcion);
+        }
+        echo "0 . Salir\n";
+        echo "=================================\n";
+        echo "=========== Instiform ===========\n";
+        echo "\n";
+    }
+    
+}
+
