05bd2e18a24c64212feaea33028b7c98c894d1da
andreazunino <108498948+andreazunino@users.noreply.github.com>
2024-02-23 10:56:17 -0300

    Se agregan a modelo las clases: curso, estudiante, profesor, gestionCurso e inscripciones con sus respectivos cambios

---
+++ Modelo/estudiante.php
@@ -0,0 +1,68 @@
+<?php 
+
+class Estudiante {
+    private $nombre;
+    private $apellido;
+    private $dni;
+    private $email;
+
+    public function __construct($nombre, $apellido, $dni, $email){
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->dni = $dni;
+        $this->email = $email;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+}
